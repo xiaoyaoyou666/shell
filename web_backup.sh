1010f3510c6def5c95ec4180e50ed281ea39ebdd
DESKTOP-UJGTEQK\zc123456 <944481698@qq.com>
2021-07-07 15:14:59 +0800

    提交测试

---
+++ web_backup.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+WEBBACK_DIR="/mnt/web_backup"
+
+#格式化日期,备份文件时用日期来做文件名的
+DATE=`date +%Y%m%d-%H%M%S`
+#保存日期
+DAYS=10
+
+############ www info ######################
+#WEB目录
+WEBSITE_DIR="/data/"
+TARGET_DIR="web"
+
+# 排除某些目录(日志和缓存),exclude.txt文件中写入   
+#/data/web/Runtime 
+#/data/web/logs
+#指定www备份文件的前缀
+
+#开始备份网站目录，备份过程同上
+cd $WEBSITE_DIR
+tar zcvf ${WEBBACK_DIR}/${DATE}.tar.gz ${TARGET_DIR}
+if [ $? -eq 0 ]; then
+        echo "succed $DATE" >> /var/log/web_backup.log
+else
+        echo "failed $DATE" >> /var/log/web_backup.log
+fi
+
+
+#远程备份 需要配置ssh的无密码传输
+scp /mnt/web_backup/${DATE}.tar.gz root@39.105.185.143:/mnt/
+
+
+#只保留指定时间内的文件
+find ${WEBBACK_DIR} -name "*.tar.gz" -type f -mtime +${DAYS} -exec rm {} \;
